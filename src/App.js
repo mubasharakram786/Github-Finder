5393e5ee14083aa659978bf236a2a2531ddcfcd6
makram786 <mubashar1418@gmail.com>
2022-04-21 14:36:39 +0500

    feat: burger builder container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import Layout from './Component/Layout/Layout';
+import BurgerBuilder  from './Container/BurgerBuilder/BurgerBuilder';
 class App extends Component {
   render() {
     return (
       <Layout>
-        <p>Test</p>
+       <BurgerBuilder/>
       </Layout>
 
     )
---
+++ src/Container/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Auxil from '../../hoc/Auxil'
+export default class BurgerBuilder extends Component {
+  render() {
+    return (
+        <Auxil>
+      <div>BurgerBuilder</div>
+      <div>Build Control</div>
+      </Auxil>
+    )
+  }
+}
